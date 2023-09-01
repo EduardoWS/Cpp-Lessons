65645acad56754e4ddf0ba861dfb79f0d91065f7
EduardoWS <eduardorr.ws@gmail.com>
2023-09-01 01:29:20 -0300

    first

---
+++ aulas/aula04-variaveis.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    double salario = 0;
+
+    cout << "Digite o seu salário: R$";
+    cin >> salario;
+
+    return 0;
+}
---
+++ aulas/aula05-constantes.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+#define PI 3.1415
+#define PRINTAR cout << "============\n"   //associando um comando na constante
+
+int main(){
+
+    cout << PI << "\n\n";
+    PRINTAR;
+
+    return 0;
+}
